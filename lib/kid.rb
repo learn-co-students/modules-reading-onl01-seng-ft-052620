ace06975accb44e3413c69cf93be15a5e61ba78f
francepants <franceline90@gmail.com>
2020-06-11 21:25:25 -0500

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,23 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+# To lend our two classes all of the methods of the `Dance` module, we use the `include` keyword: 
+# include Dance
+
+# attr_accessor :name
+
+# def initialize(name)
+#   @name = name
+# end
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+end
